2f1869b2a911a1d74556ca79f96de96e90335c74
aarjay123 <76173037+aarjay123@users.noreply.github.com>
2023-01-17 21:15:40 +0000

    Add files via upload

--- scripts/links.js
+++ scripts/links.js
@@ -1,17 +1,132 @@
+//RESTAURANT LINKS
+
+function restaurant_back() {
+	window.location.href = "restaurant.html";
+}
+
+function restaurant_breakfastTop() {
+	window.location.href = "breakfast.html#";
+}
+
+			//---restaurant menu download---
+function restaurant_menudownload() {
+	window.open('https://drive.google.com/uc?export=download&id=1Iwnhx3FnYckaj07sDKBWGliXZRhA5HxU', '_blank');
+}
+
+			//---buttons on main page---
+function restaurant_breakfast() {
+	window.location.href = "breakfast.html";
+}
+
+function restaurant_foodordering() {
+	window.location.href = "foodordering.html";
+}
+
+function restaurant_menus() {
+	window.location.href = "menus.html";
+}
+
+function restaurant_locations() {
+	window.location.href = "locations.html";
+}
+
+function restaurant_offers() {
+	window.location.href = "offers.html";
+}
+
+//HELP LINKS
+
+function help_back() {
+	window.location.href = "help.html";
+}
+
+			//---buttons on main page---
+function help_checkin() {
+	window.location.href = "checkinHELP.html";
+}
+
+function help_key() {
+	window.location.href = "keyHELP.html";
+}
+
+function help_location() {
+	window.location.href = "locationHELP.html";
+}
+
+function help_breakfast() {
+	window.location.href = "breakfastHELP.html";
+}
+
+function help_rmservice() {
+	window.location.href = "rmserviceHELP.html";
+}
+
+function help_forms() {
+	window.location.href = "formsHELP.html";
+}
+
+//HELP-MORE LINKS
+
+function helpmore_back() {
+	window.location.href = "help-more.html";
+}
+
+			//---buttons on main page---
+
+function helpmore_help() {
+	window.location.href = "help.html";
+}
+
+function helpmore_tc() {
+	window.location.href = "terms-conditions.html";
+}
+
+//HOTEL ACTIVITIES LINKS
+
+function ha_key() {
+	window.location.href = "roomkey/keymain.html";
+}
+
+function ha_sn() {
+	window.location.href = "hotelactivities/sn.html";
+}
+
+function ha_we() {
+	window.location.href = "hotelactivities/we.html";
+}
+
+function ha_hc() {
+	window.location.href = "hotelactivities/hc.html";
+}
+
+//SETTINGS LINKS
+
+function settings_updates() {
+	window.open('https://sites.google.com/view/thcmobile-by-thc/download', '_blank');
+}
+
+function settings_changelog() {
+	window.open('https://sites.google.com/view/thcmobile-by-thc/download#h.evwm3pfxwkdn', '_blank');
+}
+
+function settings_website() {
+	window.open('https://sites.google.com/view/thehighlandcafe', '_blank');
+}
+
 //JOURNEY LINKS
 
 function journey_flights() {
-	window.location.href = "flights.html"
+	window.location.href = "flights.html";
 }
 
 function journey_carHire() {
-	window.location.href = "carhire.html"
+	window.location.href = "carhire.html";
 }
 
 function journey_taxi() {
-	window.location.href = "taxi.html"
+	window.location.href = "taxi.html";
 }
 
 function journey_back() {
-	window.location.href = "journey.html"
+	window.location.href = "journey.html";
 }
