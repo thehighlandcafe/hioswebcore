4a77713bcf092f5a74b55b5a9ba5e8675a16b0b9
aarjay123 <76173037+aarjay123@users.noreply.github.com>
2023-01-17 16:20:27 +0000

    Add files via upload

---
+++ scripts/links.js
@@ -0,0 +1,17 @@
+//JOURNEY LINKS
+
+function journey_flights() {
+	window.location.href = "flights.html"
+}
+
+function journey_carHire() {
+	window.location.href = "carhire.html"
+}
+
+function journey_taxi() {
+	window.location.href = "taxi.html"
+}
+
+function journey_back() {
+	window.location.href = "journey.html"
+}
